f425f44c0a22eee094dc8d3e6e584033f3169dd4
hykar <aandreevh@gmail.com>
2019-06-26 13:04:13 +0300

    Init

---
+++ cmake-build-debug/CMakeFiles/consult.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/consult.dir/main.cpp.o"
+  "consult.pdb"
+  "consult"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/consult.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
